14c6ef0130d2d7968383265b7e0642c6f526daf5
LDawns <250858445@qq.com>
2021-06-24 17:40:01 +0800

    x

--- test_hello.py
+++ test_hello.py
@@ -6,6 +6,8 @@ import hello
 class TestMethods(unittest.TestCase):
     def test_add(self):
         self.assertEqual(hello.say1(), "hello")
+    def test_say(self):
+        self.assertEqual(hello.say2(), "world")
 
 
 if __name__ == '__main__':
